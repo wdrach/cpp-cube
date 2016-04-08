2d540b02c1e7ecc614badf91f1a6d9a4ede8c2e7
Will Drach <will.drach@gmail.com>
2016-04-08 11:48:08 -0600

    added basic state instructions

--- cube.h
+++ cube.h
@@ -1,6 +1,59 @@
+/* The state is in the following format:
+ * It's an array with each cube face taking up 9 consecutive units
+ * [1, 2, 3, 4, 5, 6, 7, 8, 9] ==
+ * [ 1, 2, 3,
+ *   4, 5, 6
+ *   7, 8, 9 ]
+ *
+ * so 1 in that array is the top left corner, and it is laid out
+ * like a number pad.
+ *
+ * The order of faces is U, L, F, R, B, D, same order used for Speffz
+ *
+ * Oh, and by the way, that's how it works, with Speffz, but Speffz doesn't
+ * care about centers and we do, so we label those A-F in the order above,
+ * so a solved cube will have a state like this:
+ *[
+ * a, A, b,
+ * D, A, B,
+ * d, C, c,
+ *
+ * e, E, f,
+ * H, B, F,
+ * h, G, g,
+ *
+ * i, I, j,
+ * L, C, J,
+ * l, K, k,
+ *
+ * m, M, n,
+ * P, D, N,
+ * p, O, o,
+ *
+ * q, Q, r,
+ * T, E, R,
+ * t, S, s,
+ *
+ * u, U, v,
+ * X, F, V,
+ * x, W, w
+ *]
+ *
+ * This makes it easy to catch things like cube rotations, which is easy
+ * to handle for a human (you just look at it differently), but not easy
+ * for a robot to handle, which is the end goal of this simulator
+ *
+ * Eventually this lib will be able to handle just taking colors, but
+ * this is how it be for the time being (mostly because it's easier to
+ * solve a cube using the PIECES instead of the colors)
+ */
+
+
 class cube {
+  //the user should not be able to directly influence the state
   char state[54];
   public:
+    //constructor
     cube(int start_state[54]);
     int* getState();
 
